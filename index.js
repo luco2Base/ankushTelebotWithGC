b1643ade61b5f8bc3db2d0f60c7a91014c2c34fd
luco2Base <onlyjacyhere@gmail.com>
2024-09-06 10:56:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ const createHealthCheckServer = () => {
 // Self-ping to prevent app from sleeping
 const keepAppAwake = () => {
   setInterval(() => {
-    https.get("https://telebotankush6-gmjkpy5h.b4a.run/health", (res) => { // Changed from http to https
+    https.get("https://telebotankush7-jjrkjigr.b4a.run/health", (res) => { // Changed from http to https
       console.log("Self-ping: ", res.statusCode);
     }).on("error", (err) => {
       console.error("Error in self-ping: ", err.message);
